e80af4b3d755ec6fb16adf6640afe306907a5b33
TuanNM <TuanNM@TUANNM>
2023-09-06 10:17:40 +0700

    Add project files.

---
+++ TestOOPMVC/Model/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestOOPMVC.Sevice;
+
+namespace TestOOPMVC.Model
+{
+    public abstract class Cat
+    {
+        public int ID { get; set; }
+        public string TenMeo { get; set; }
+        public int Tuoi { get; set; }
+        public string GioiTinh { get; set; }
+        public Kieukeu Kieukeu { get; set; }
+
+        public string Keu()
+        {
+            return Kieukeu.Keu();
+        }
+    }
+}
